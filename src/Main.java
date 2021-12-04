ebd485efaf113f6a3208ef658ea4dee70878490e
Nassim LOUNADI <nassimlnd37@gmail.com>
2021-12-04 11:50:51 +0100

    Add the file system and the id system

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Patient patient1 = new Patient("Test1");
+        Patient patient2 = new Patient("Nassim");
+
+    }
+}
--- src/Patient.java
+++ src/Patient.java
@@ -1,4 +1,7 @@
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Patient {
 
@@ -9,13 +12,52 @@ public class Patient {
     private ArrayList<Consultation> listeConsultationPatient;
     int cb = 0;
 
+    static ArrayList<Patient> listePatient = new ArrayList<Patient>();
+
     // Constructeur
 
     public Patient(String nom) {
-        cb++;
-        this.id = cb;
-        this.nom = nom;
-        this.listeConsultationPatient = new ArrayList<Consultation>();
+        //cb++;
+        //this.id = cb;
+        try {
+            this.setId();
+            this.nom = nom;
+            this.listeConsultationPatient = new ArrayList<Consultation>();
+            File file = new File("patient.txt");
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileReader fileReader = new FileReader(file.getAbsoluteFile());
+            Scanner sc = new Scanner(fileReader);
+
+            boolean contains = false;
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                if (line.toLowerCase().contains(this.nom.toLowerCase())) {
+                    contains = true;
+                }
+            }
+
+            fileReader.close();
+
+            if (contains == false) {
+                FileWriter fileWriter = new FileWriter(file.getAbsoluteFile(), true);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write(this.getId() + "-" + this.getNom() + "\n");
+                bufferedWriter.close();
+                fileWriter.close();
+            }
+            else {
+                System.out.println("Patient déjà existant.");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     // Getters and setters
@@ -24,8 +66,34 @@ public class Patient {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId() throws IOException {
+        FileReader fileReader;
+        Scanner sc;
+        try {
+            File file = new File("patient.txt");
+
+            if (file.exists()) {
+                fileReader = new FileReader("patient.txt");
+                sc = new Scanner(fileReader);
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+                    String[] split = line.split("-");
+                    try {
+                        this.id = Integer.parseInt(split[0]) + 1;
+                    } catch (NumberFormatException e) {
+                        this.id = 1;
+                    }
+                }
+            } else {
+                file.createNewFile();
+                this.id = 1;
+            }
+        }
+        catch (FileNotFoundException exception) {
+
+        }
+
+
     }
 
     public String getNom() {
