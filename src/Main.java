d5271aaa37d9de481a75ceeb16def9a88dc1a4af
Nassim LOUNADI <nassimlnd37@gmail.com>
2021-12-20 20:22:03 +0100

    Supprimer

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,6 @@ public class Main {
     public static void main(String[] args) throws PatientNotFoundException {
 
         //Patient patient1 = new Patient("Nassim", "02/08/2002", "1234567891234");
-        /*Patient patient2 = new Patient("Magomed", "02/08/2002", "1234567891234");
-        for (Patient patient1 : Patient.getListePatient()) {
-            System.out.println(patient1.getNom());
-        }*/
-
-        //Patient.getPatient("1234567891234");
-
-        //Consultation consultation = new Consultation("test", "test", patient, "test");
 
         try {
             Frame frame = new Frame();
